ad2a9a4f1ba56244e834e7c32e866b6549e026d2
Özay Melih Yıldız <ozay.m.yildiz@gmail.com>
2023-09-16 23:48:03 +0300

    Initial commit

---
+++ Class(Again)/Class(Again)/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAgain
+{
+    public class Car
+    {
+        public int numberofDoor;
+        public string model;
+        public string colour;
+
+        public Car(int numberofDoor, string colour, string model)
+        {
+            this.numberofDoor = numberofDoor;
+            this.colour = colour;
+            this.model = model;
+        }
+
+        public void runEngine()
+        {
+            Console.WriteLine("The engine is running...");
+        }
+        public void lockDoor()
+        {
+            Console.WriteLine("The doors are locking...");
+        }
+    }
+}
---
+++ Class(Again)/Class(Again)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(4, "Red", "Renauld");
+            
+            car.runEngine();
+            car.lockDoor();
+
+            Console.WriteLine("Car model is " + car.model);
+            Console.WriteLine("Car colour is " + car.colour);
+
+            Console.ReadLine();
+        }
+    }
+}
